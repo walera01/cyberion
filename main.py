d4c0659a55c19ee530fb85204997b476708ce94e
Влад <wladislaw1004@gmail.com>
2022-05-10 20:26:00 +0300

    10.05

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(~100)
+from pixellib.instance import instance_segmentation
+
+def objectt_detection_image():
+    segment_image= instance_segmentation( )
+    segment_image.load_model("mask_rcnn_coco.h5")
+
+    segment_image.segmentImage(
+        image_path="caption.jpg",
+        output_image_name="output.jpg"
+    )
+    
+def main():
+    objectt_detection_image()
+    
+    
+if __name__ == "__main__":
+    main()
