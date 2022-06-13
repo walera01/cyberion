55334fff5b5a460ae15809afdd428bcce343e4e3
pitonshcik <walera01>
2022-06-13 20:53:34 +0300

    13.06 подключение кэша

--- project/drugs/urls.py
+++ project/drugs/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.cache import cache_page
+
 from .views import *
 
 urlpatterns = [
     path('drugi/',Drug.as_view(), name='drug'),                                 #все товары
-    path('drugi/find/',Drug.as_view(), name='drugfind'),                       #поиск и поиск по цене
+    path('drugi/find/',cache_page(100)(Drug.as_view()), name='drugfind'),                       #поиск и поиск по цене
     path('category/<slug:category_slug>/find/',Drug.as_view(), name='catfind'),
     path('subcategory/<slug:subcategory_slug>/find/',Drug.as_view(), name='subcatfind'),
 
