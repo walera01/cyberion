7dd532c19d234243cf4d01aacd8af63d0bd9257d
walera01 <wladislaw1004@gmail.com>
2022-06-15 14:51:56 +0300

    14.06 ++

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,13 +16,3 @@ urlpatterns = [
 #                   ] + urlpatterns
 #
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-from django.views.static import serve as mediaserve
-from django.conf.urls import url
-
-
-urlpatterns += [
-    url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
-        mediaserve, {'document_root': settings.MEDIA_ROOT}),
-    url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
-        mediaserve, {'document_root': settings.STATIC_ROOT}),
-]
